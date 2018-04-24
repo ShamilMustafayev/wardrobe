c3ef8b575b5ab614dbc9b3d5a17b1dc6c38ca11f
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-04-24 11:55:22 +0200

    Mapping the WardrobeController

--- src/main/java/com/capgemini/wardrobe/model/Wardrobe.java
+++ src/main/java/com/capgemini/wardrobe/model/Wardrobe.java
@@ -1,4 +1,17 @@
 package com.capgemini.wardrobe.model;
 
 public class Wardrobe {
+    private String name;
+
+    public Wardrobe(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
