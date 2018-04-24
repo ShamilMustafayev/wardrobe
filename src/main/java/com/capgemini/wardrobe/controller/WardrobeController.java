becb93825ffe3f7cfd55baabfc160412172cf359
ShamilMustafayev <38105274+ShamilMustafayev@users.noreply.github.com>
2018-04-24 12:03:29 +0200

    Index.html, registry.js, buttons
    Created related index, js files and dummy buttons

--- src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
+++ src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
@@ -1,5 +1,8 @@
 package com.capgemini.wardrobe.controller;
 
+import com.capgemini.wardrobe.model.Wardrobe;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/register")
 
 public class WardrobeController {
+
+    @GetMapping("/test/{name}")
+    public Wardrobe test(@PathVariable String name)
+    {
+        return new Wardrobe (name);
+    }
 
 }
